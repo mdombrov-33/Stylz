e69165bf9fa677cf719f3f807e843763534dd609
Maksym Dombrov <maksymdombrov@gmail.com>
2024-09-16 14:41:29 +0300

    add small versions of bg images

--- src/assets/showcase/index.js
+++
@@ -1,8 +0,0 @@
-export { default as showcase1 } from "./showcase1.jpg";
-export { default as showcase2 } from "./showcase2.jpg";
-export { default as showcase3 } from "./showcase3.jpg";
-export { default as showcase4 } from "./showcase4.jpg";
-export { default as showcase5 } from "./showcase5.jpg";
-export { default as showcase6 } from "./showcase6.jpg";
-export { default as showcase7 } from "./showcase7.jpg";
-export { default as showcase8 } from "./showcase8.jpg";
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,11 +1,19 @@
 import showcase1 from "../assets/showcase/showcase1.jpg";
+import showcase1small from "../assets/showcase/showcase1small.jpg";
 import showcase2 from "../assets/showcase/showcase2.jpg";
+import showcase2small from "../assets/showcase/showcase2small.jpg";
 import showcase3 from "../assets/showcase/showcase3.jpg";
+import showcase3small from "../assets/showcase/showcase3small.jpg";
 import showcase4 from "../assets/showcase/showcase4.jpg";
+import showcase4small from "../assets/showcase/showcase4small.jpg";
 import showcase5 from "../assets/showcase/showcase5.jpg";
+import showcase5small from "../assets/showcase/showcase5small.jpg";
 import showcase6 from "../assets/showcase/showcase6.jpg";
+import showcase6small from "../assets/showcase/showcase6small.jpg";
 import showcase7 from "../assets/showcase/showcase7.jpg";
+import showcase7small from "../assets/showcase/showcase7small.jpg";
 import showcase8 from "../assets/showcase/showcase8.jpg";
+import showcase8small from "../assets/showcase/showcase8small.jpg";
 import useIntersectionObserver from "../hooks/useIntersectionObserver";
 
 function Showcase() {
@@ -25,7 +33,9 @@ function Showcase() {
           ref={ref1}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-center"
           style={{
-            backgroundImage: isIntersecting1 ? `url(${showcase1})` : "none",
+            backgroundImage: isIntersecting1
+              ? `url(${showcase1})`
+              : `url(${showcase1small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -33,7 +43,9 @@ function Showcase() {
           ref={ref2}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center md:bg-center xl:bg-[center_-3rem]"
           style={{
-            backgroundImage: isIntersecting2 ? `url(${showcase2})` : "none",
+            backgroundImage: isIntersecting2
+              ? `url(${showcase2})`
+              : `url(${showcase2small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -41,7 +53,9 @@ function Showcase() {
           ref={ref3}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting3 ? `url(${showcase3})` : "none",
+            backgroundImage: isIntersecting3
+              ? `url(${showcase3})`
+              : `url(${showcase3small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -49,7 +63,9 @@ function Showcase() {
           ref={ref4}
           className="h-[50dvh] border-b-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting4 ? `url(${showcase4})` : "none",
+            backgroundImage: isIntersecting4
+              ? `url(${showcase4})`
+              : `url(${showcase4small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -57,7 +73,9 @@ function Showcase() {
           ref={ref5}
           className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting5 ? `url(${showcase5})` : "none",
+            backgroundImage: isIntersecting5
+              ? `url(${showcase5})`
+              : `url(${showcase5small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -65,7 +83,9 @@ function Showcase() {
           ref={ref6}
           className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting6 ? `url(${showcase6})` : "none",
+            backgroundImage: isIntersecting6
+              ? `url(${showcase6})`
+              : `url(${showcase6small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -73,7 +93,9 @@ function Showcase() {
           ref={ref7}
           className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting7 ? `url(${showcase7})` : "none",
+            backgroundImage: isIntersecting7
+              ? `url(${showcase7})`
+              : `url(${showcase7small})`,
             filter: "brightness(70%)",
           }}
         ></div>
@@ -81,7 +103,9 @@ function Showcase() {
           ref={ref8}
           className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: isIntersecting8 ? `url(${showcase8})` : "none",
+            backgroundImage: isIntersecting8
+              ? `url(${showcase8})`
+              : `url(${showcase8small})`,
             filter: "brightness(70%)",
           }}
         ></div>
