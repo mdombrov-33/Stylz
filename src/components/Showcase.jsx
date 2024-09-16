2ef76cc0fc19ff5615e19119089bd9fc6e50544f
Maksym Dombrov <maksymdombrov@gmail.com>
2024-09-16 14:08:40 +0300

    add intersection observer hook for image performance

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,71 +1,87 @@
-import {
-  showcase1,
-  showcase2,
-  showcase3,
-  showcase4,
-  showcase5,
-  showcase6,
-  showcase7,
-  showcase8,
-} from "../assets/showcase";
+import showcase1 from "../assets/showcase/showcase1.jpg";
+import showcase2 from "../assets/showcase/showcase2.jpg";
+import showcase3 from "../assets/showcase/showcase3.jpg";
+import showcase4 from "../assets/showcase/showcase4.jpg";
+import showcase5 from "../assets/showcase/showcase5.jpg";
+import showcase6 from "../assets/showcase/showcase6.jpg";
+import showcase7 from "../assets/showcase/showcase7.jpg";
+import showcase8 from "../assets/showcase/showcase8.jpg";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
 
 function Showcase() {
+  const [ref1, isIntersecting1] = useIntersectionObserver();
+  const [ref2, isIntersecting2] = useIntersectionObserver();
+  const [ref3, isIntersecting3] = useIntersectionObserver();
+  const [ref4, isIntersecting4] = useIntersectionObserver();
+  const [ref5, isIntersecting5] = useIntersectionObserver();
+  const [ref6, isIntersecting6] = useIntersectionObserver();
+  const [ref7, isIntersecting7] = useIntersectionObserver();
+  const [ref8, isIntersecting8] = useIntersectionObserver();
+
   return (
     <div className="grid">
       <div className="grid grid-cols-4 sm:grid-cols-4 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-2">
         <div
+          ref={ref1}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-center"
           style={{
-            backgroundImage: `url(${showcase1})`,
+            backgroundImage: isIntersecting1 ? `url(${showcase1})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
+          ref={ref2}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center md:bg-center xl:bg-[center_-3rem]"
           style={{
-            backgroundImage: `url(${showcase2})`,
+            backgroundImage: isIntersecting2 ? `url(${showcase2})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
+          ref={ref3}
           className="h-[50dvh] border-b-2 border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase3})`,
+            backgroundImage: isIntersecting3 ? `url(${showcase3})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
+          ref={ref4}
           className="h-[50dvh] border-b-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase4})`,
+            backgroundImage: isIntersecting4 ? `url(${showcase4})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
+          ref={ref5}
           className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase5})`,
+            backgroundImage: isIntersecting5 ? `url(${showcase5})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
-          className="h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
+          ref={ref6}
+          className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase6})`,
+            backgroundImage: isIntersecting6 ? `url(${showcase6})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
-          className="h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center"
+          ref={ref7}
+          className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase7})`,
+            backgroundImage: isIntersecting7 ? `url(${showcase7})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
         <div
-          className="h-[50dvh] border-stone-950 bg-cover bg-center sm:bg-center md:bg-center xl:bg-center 2xl:bg-[center_-4.5rem]"
+          ref={ref8}
+          className="md-block h-[50dvh] border-r-2 border-stone-950 bg-cover bg-center sm:bg-center lg:bg-top"
           style={{
-            backgroundImage: `url(${showcase8})`,
+            backgroundImage: isIntersecting8 ? `url(${showcase8})` : "none",
             filter: "brightness(70%)",
           }}
         ></div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,15 @@
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="h-screen">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum architecto
+      nemo tempore aspernatur voluptas distinctio, possimus non cum
+      consequuntur, sunt tenetur aliquam alias reprehenderit officiis velit
+      perspiciatis. Nesciunt, debitis consectetur. Ullam dolores ex ipsum porro
+      quia unde! Dolorum consectetur, facilis odio exercitationem quae
+      distinctio laborum, placeat nihil autem reiciendis cumque fuga repellendus
+      quidem similique magnam debitis unde dolores voluptatibus adipisci?
+    </div>
+  );
 }
 
 export default Contact;
