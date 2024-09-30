2976219fafed77a717a4b6a3bb43fa521198882b
Maksym Dombrov <maksymdombrov@gmail.com>
2024-09-30 23:48:53 +0300

    update register attributes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,62 +9,68 @@ function Register() {
         </h2>
         <p className="py-2">It's quick and easy</p>
         <form className="flex w-96 flex-col items-center">
-          <label
-            className="w-96 justify-start font-redHatDisplay font-bold"
-            htmlFor="full-name"
-          >
-            Full Name
-          </label>
-          <input
-            className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
-            type="text"
-            placeholder="John Doe"
-            id="full-name"
-            autoComplete="name"
-          />
-          <label
-            className="w-96 justify-start font-redHatDisplay font-bold"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
-            type="email"
-            placeholder="stylzmc@gmail.com"
-            id="email"
-            autoComplete="username"
-          />
-          <label
-            className="w-96 justify-start font-redHatDisplay font-bold"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
-            type="password"
-            placeholder="********"
-            id="password"
-            autoComplete="new-password"
-          />
-          <label
-            className="w-96 justify-start font-redHatDisplay font-bold"
-            htmlFor="confirm-password"
-          >
-            Confirm Password
-          </label>
-          <input
-            className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
-            type="password"
-            placeholder="********"
-            id="confirm-password"
-            autoComplete="new-password"
-          />
-
-          <button className="btn my-2 w-full rounded-lg bg-accent py-2 text-lg text-white">
-            Register
-          </button>
+          <fieldset className="w-full">
+            <legend className="sr-only">Registration Form</legend>
+            <label
+              className="w-96 justify-start font-redHatDisplay font-bold"
+              htmlFor="full-name"
+            >
+              Full Name
+            </label>
+            <input
+              className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
+              type="text"
+              placeholder="John Doe"
+              id="full-name"
+              autoComplete="name"
+              required
+            />
+            <label
+              className="w-96 justify-start font-redHatDisplay font-bold"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
+              type="email"
+              placeholder="stylzmc@gmail.com"
+              id="email"
+              autoComplete="username"
+              required
+            />
+            <label
+              className="w-96 justify-start font-redHatDisplay font-bold"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
+              type="password"
+              placeholder="********"
+              id="password"
+              autoComplete="new-password"
+              required
+            />
+            <label
+              className="w-96 justify-start font-redHatDisplay font-bold"
+              htmlFor="confirm-password"
+            >
+              Confirm Password
+            </label>
+            <input
+              className="my-2 w-full rounded-lg border border-stone-950 px-4 py-2 placeholder-gray-500"
+              type="password"
+              placeholder="********"
+              id="confirm-password"
+              autoComplete="new-password"
+              required
+            />
+            <button className="btn my-2 w-full rounded-lg bg-accent py-2 text-lg text-white">
+              Register
+            </button>
+          </fieldset>
         </form>
         <div className="mt-14">
           <ReturnBtn />
