c10ae87825e37ea3656621fd9df5ea6d0ac2725a
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-03 21:37:32 +0300

    rewrite contact us page

--- src/pages/ContactLayout.jsx
+++ src/pages/ContactLayout.jsx
@@ -3,43 +3,46 @@ import { Outlet } from "react-router-dom";
 
 function ContactLayout() {
   return (
-    <section className="grid h-full py-10 pl-4 md:mr-12 md:grid-cols-3 lg:items-center lg:justify-center">
-      <section>
-        <h1 className="text-3xl font-bold uppercase tracking-wider">
-          contact us
-        </h1>
-        <h3 className="py-6 font-bold uppercase"> customer care</h3>
-        <nav className="w-max">
-          <ul className="grid grid-cols-3 gap-x-20 sm:grid-cols-4 md:grid-cols-1">
-            {[
-              { to: "contactinfo", label: "Contact us" },
-              { to: "payment", label: "Payment" },
-              { to: "ordering", label: "Ordering" },
-              { to: "shipping", label: "Shipping" },
-              { to: "returns", label: "Returns" },
-              { to: "sizeguide", label: "Size Guide" },
-              { to: "whystylz", label: "Why Stylz" },
-              { to: "feedback", label: "Feedback" },
-              { to: "faqs", label: "FAQs" },
-            ].map((link) => (
-              <NavLink
-                key={link.to}
-                to={link.to}
-                className={({ isActive }) =>
-                  `mt-2 w-max font-redHatDisplay transition-all duration-150 ${
-                    isActive ? "text-accent" : "hover:text-accent"
-                  }`
-                }
-              >
-                {link.label}
-              </NavLink>
-            ))}
-          </ul>
-        </nav>
+    <>
+      <section className="grid h-full md:grid-cols-3">
+        <div className="ml-12 mt-12 md:col-span-1">
+          <h1 className="text-3xl font-bold uppercase tracking-wider">
+            contact us
+          </h1>
+          <h3 className="py-6 font-bold uppercase"> customer care</h3>
+          <nav className="w-max">
+            <ul className="grid grid-cols-3 gap-x-20 sm:grid-cols-4 md:grid-cols-1">
+              {[
+                { to: "contactinfo", label: "Contact us" },
+                { to: "payment", label: "Payment" },
+                { to: "ordering", label: "Ordering" },
+                { to: "shipping", label: "Shipping" },
+                { to: "returns", label: "Returns" },
+                { to: "sizeguide", label: "Size Guide" },
+                { to: "whystylz", label: "Why Stylz" },
+                { to: "feedback", label: "Feedback" },
+                { to: "faqs", label: "FAQs" },
+              ].map((link) => (
+                <NavLink
+                  key={link.to}
+                  to={link.to}
+                  className={({ isActive }) =>
+                    `mt-2 w-max font-redHatDisplay transition-all duration-150 ${
+                      isActive ? "text-accent" : "hover:text-accent"
+                    }`
+                  }
+                >
+                  {link.label}
+                </NavLink>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className="md:col-span-2">
+          <Outlet />
+        </div>
       </section>
-
-      <Outlet />
-    </section>
+    </>
   );
 }
 
--- src/pages/ContactRoutes/FAQs.jsx
+++ src/pages/ContactRoutes/FAQs.jsx
@@ -1,5 +1,9 @@
 function FAQs() {
-  return <div>faqs</div>;
+  return (
+    <section className="grid-cols-3 items-center justify-center">
+      <h2>Do you have questions?</h2>
+    </section>
+  );
 }
 
 export default FAQs;
