a07f790016048028243d7d3f970466092400be72
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-03 22:01:57 +0300

    add handler for whatsapp button

--- src/pages/ContactRoutes/ContactInfo.jsx
+++ src/pages/ContactRoutes/ContactInfo.jsx
@@ -1,5 +1,8 @@
 import { FaWhatsapp } from "react-icons/fa";
 function ContactInfo() {
+  const handleClick = () => {
+    window.location.href = "https://www.whatsapp.com/";
+  };
   return (
     <>
       <section>
@@ -12,7 +15,7 @@ function ContactInfo() {
               through Live Chat using button below(ENGLISH ONLY).
             </p>
             <p className="py-6 font-bold">Or Message Us on WhatsApp.</p>
-            <button className="btn w-96 md:w-72">
+            <button onClick={handleClick} className="btn w-96 md:w-72">
               <FaWhatsapp className="mr-2 text-2xl" />
               <span className="text-lg">WhatsApp</span>
               <span className="ml-5 w-20 rounded-xl border border-accent bg-accent p-2">
