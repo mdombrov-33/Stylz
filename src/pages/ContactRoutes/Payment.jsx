da8cf6919b7b4e6718ba55b72f251ce6094ea276
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-04 11:27:06 +0300

    add img preload for payment route

--- src/pages/ContactRoutes/Payment.jsx
+++ src/pages/ContactRoutes/Payment.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import visa from "../../assets/contact/visa.png";
 import americanExpress from "../../assets/contact/americanexpress.png";
 import masterCard from "../../assets/contact/mastercard.png";
@@ -8,7 +9,30 @@ import apple from "../../assets/contact/apple.png";
 import google from "../../assets/contact/google.png";
 import giro from "../../assets/contact/giro.png";
 
+const images = [
+  visa,
+  americanExpress,
+  masterCard,
+  discover,
+  jcb,
+  paypal,
+  apple,
+  google,
+  giro
+];
+
+function preloadImages(imageArray) {
+  imageArray.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+  });
+}
+
 function Payment() {
+  useEffect(() => {
+    preloadImages(images);
+  }, []);
+
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 justify-center items-center mt-12 gap-12 px-6">
       <img className="md:h-36 md:w-40 min-w-[10rem] min-h-[9rem]" loading="lazy" src={visa} alt="Visa" />
