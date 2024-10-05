d6c824b8a1796f4d7c355b53204c77b1fe9428b6
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-05 17:18:40 +0300

    fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import hero1 from "../assets/hero1.jpg";
 import hero2 from "../assets/hero2.jpg";
 import { FaArrowDown } from "react-icons/fa";
@@ -6,35 +5,18 @@ import Collab from "@/features/home/Collab";
 import Showcase from "@/features/home/Showcase";
 
 function Landing() {
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const updateHeight = () => {
-      if (sectionRef.current) {
-        sectionRef.current.style.height = `${window.innerHeight}px`;
-      }
-    };
-
-    updateHeight();
-    window.addEventListener("resize", updateHeight);
-
-    return () => {
-      window.removeEventListener("resize", updateHeight);
-    };
-  }, []);
-
   return (
     <>
-      <section className="grid h-[80dvh] grid-cols-2">
+      <section className="grid h-[80svh] grid-cols-2">
         <div
           style={{ backgroundImage: `url(${hero1})` }}
-          className="h-[80dvh] w-full bg-cover"
+          className="h-full w-full bg-cover"
         >
           aboba
         </div>
         <div
           style={{ backgroundImage: `url(${hero2})` }}
-          className="h-[80dvh] w-full bg-cover"
+          className="h-full w-full bg-cover"
         >
           aboba
         </div>
