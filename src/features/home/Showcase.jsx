2c67638e58522d0b9b39255fbe983c1d196e9351
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-05 11:07:02 +0300

    reduce showcase images number

--- src/features/home/Showcase.jsx
+++ src/features/home/Showcase.jsx
@@ -1,21 +1,15 @@
-import showcase1 from "../../assets/showcase/showcase1.jpg";
+// import showcase1 from "../../assets/showcase/showcase1.jpg";
 import showcase2 from "../../assets/showcase/showcase2.jpg";
 import showcase3 from "../../assets/showcase/showcase3.jpg";
-import showcase4 from "../../assets/showcase/showcase4.jpg";
-import showcase5 from "../../assets/showcase/showcase5.jpg";
-import showcase6 from "../../assets/showcase/showcase6.jpg";
+// import showcase4 from "../../assets/showcase/showcase4.jpg";
+// import showcase5 from "../../assets/showcase/showcase5.jpg";
+// import showcase6 from "../../assets/showcase/showcase6.jpg";
 import showcase7 from "../../assets/showcase/showcase7.jpg";
 import showcase8 from "../../assets/showcase/showcase8.jpg";
 
 function Showcase() {
   return (
-    <div className="grid grid-cols-4 md:grid-cols-4">
-      <img
-        src={showcase1}
-        className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
-        loading="lazy"
-        alt=""
-      />
+    <div className="grid h-full grid-cols-4 md:grid-cols-4">
       <img
         src={showcase2}
         className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
@@ -23,31 +17,13 @@ function Showcase() {
         alt=""
       />
       <img
-        src={showcase3}
-        className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
-        loading="lazy"
-        alt=""
-      />
-      <img
-        src={showcase4}
-        className="posit h-full min-h-96 border-b-2 border-stone-950 object-cover object-center brightness-75"
-        loading="lazy"
-        alt=""
-      />
-      <img
-        src={showcase5}
-        className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
-        loading="lazy"
-        alt=""
-      />
-      <img
-        src={showcase6}
-        className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
-        loading="lazy"
-        alt=""
-      />
-      <img
         src={showcase7}
+        className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
+        loading="lazy"
+        alt=""
+      />
+      <img
+        src={showcase3}
         className="h-full min-h-96 border-b-2 border-r-2 border-stone-950 object-cover object-center brightness-75"
         loading="lazy"
         alt=""
