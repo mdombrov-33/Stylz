3ebd2221b2ef40977baa453b795ef5486fa03e23
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-27 13:28:15 +0200

    add faqs accordion items

--- src/pages/ContactRoutes/FAQs.jsx
+++ src/pages/ContactRoutes/FAQs.jsx
@@ -1,8 +1,252 @@
 function FAQs() {
   return (
-    <section className="grid-cols-3 items-center justify-center">
-      <h2>Do you have questions?</h2>
-    </section>
+    <div className="mt-6 grid gap-6 p-6 md:grid-cols-2">
+      <div className="flex flex-col gap-4">
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            What is your return policy?
+          </div>
+          <div className="collapse-content">
+            <p>
+              We offer a 30-day return policy for all our products. Items must
+              be returned in their original condition with tags attached. Please
+              visit our Returns page for more details.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I track my order?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Once your order has been shipped, you will receive an email with a
+              tracking number. You can use this number to track your order on
+              our website or the carrier's website.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Do you offer international shipping?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we offer international shipping to many countries. Shipping
+              costs and delivery times vary depending on the destination. Please
+              check our Shipping page for more information.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How can I contact customer service?
+          </div>
+          <div className="collapse-content">
+            <p>
+              You can contact our customer service team via email at
+              support@stylz.com or by phone at (123) 456-7890. Our team is
+              available Monday to Friday, 9 AM to 5 PM.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            What payment methods do you accept?
+          </div>
+          <div className="collapse-content">
+            <p>
+              We accept various payment methods including credit/debit cards,
+              PayPal, and Apple Pay. All transactions are securely processed.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I care for my clothing?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Each product comes with specific care instructions on the label.
+              Generally, we recommend washing in cold water and air drying to
+              maintain the quality of the fabric.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Can I change or cancel my order?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Orders can be changed or canceled within 24 hours of placing them.
+              Please contact our customer service team as soon as possible to
+              make any changes.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Do you offer gift cards?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we offer gift cards in various denominations. They can be
+              purchased on our website and are delivered electronically.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I use a discount code?
+          </div>
+          <div className="collapse-content">
+            <p>
+              You can enter your discount code at checkout. Simply type the code
+              into the designated field and click "Apply" to see the discount
+              reflected in your total.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            What sizes do you offer?
+          </div>
+          <div className="collapse-content">
+            <p>
+              We offer a wide range of sizes from XS to XXL. Please refer to our
+              Size Guide for detailed measurements and fit information.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Are your products ethically made?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we are committed to ethical manufacturing practices. Our
+              products are made in factories that adhere to fair labor standards
+              and environmental regulations.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I sign up for your newsletter?
+          </div>
+          <div className="collapse-content">
+            <p>
+              You can sign up for our newsletter by entering your email address
+              in the subscription box at the bottom of our website. You'll
+              receive updates on new arrivals, sales, and exclusive offers.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Do you offer custom tailoring?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Currently, we do not offer custom tailoring services. However, we
+              provide detailed size guides to help you find the perfect fit.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Can I return sale items?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Sale items are final sale and cannot be returned or exchanged.
+              Please make sure to review the product details and sizing
+              information before making a purchase.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I create an account?
+          </div>
+          <div className="collapse-content">
+            <p>
+              You can create an account by clicking on the "Sign Up" button at
+              the top of our website. Follow the prompts to enter your
+              information and set up your account.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            What should I do if I receive a damaged item?
+          </div>
+          <div className="collapse-content">
+            <p>
+              If you receive a damaged item, please contact our customer service
+              team immediately. We will arrange for a replacement or refund as
+              soon as possible.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            How do I leave a review?
+          </div>
+          <div className="collapse-content">
+            <p>
+              You can leave a review by visiting the product page and clicking
+              on the "Write a Review" button. We appreciate your feedback and
+              use it to improve our products and services.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            Do you offer price adjustments?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, we offer price adjustments within 14 days of purchase. If an
+              item you purchased goes on sale, please contact our customer
+              service team for a price adjustment.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
