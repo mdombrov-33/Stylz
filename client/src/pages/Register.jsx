a332d9257e5db7620d165a047887045f59fb8b50
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-01 20:01:23 +0200

    check auth

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,27 @@
+import { useForm } from "react-hook-form";
 import ReturnBtn from "@/components/ReturnBtn";
+import axios from "axios";
+import { redirect } from "react-router-dom";
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const res = await axios.post(
+        " https://fba76vjk6e.execute-api.eu-north-1.amazonaws.com/prod/register",
+        data
+      ); // or the full URL
+      console.log(res.data);
+      redirect("/login"); // Redirect after successful registration
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <section className="flex h-screen items-center justify-center">
       <div className="mt-20 flex flex-col items-center justify-center">
@@ -8,7 +29,10 @@ function Register() {
           Create your account
         </h2>
         <p className="py-2">It&apos;s quick and easy</p>
-        <form className="flex w-96 flex-col items-center" method="POST">
+        <form
+          className="flex w-96 flex-col items-center"
+          onSubmit={handleSubmit(onSubmit)} // Use handleSubmit from React Hook Form
+        >
           <fieldset className="w-full">
             <legend className="sr-only">Registration Form</legend>
             <label
@@ -22,10 +46,11 @@ function Register() {
               type="text"
               placeholder="John Doe"
               id="full_name"
-              name="full_name"
-              autoComplete="name"
-              required
+              {...register("full_name", { required: "Full Name is required" })} // Register the input
             />
+            {errors.full_name && (
+              <span className="text-red-500">{errors.full_name.message}</span>
+            )}
             <label
               className="w-96 justify-start font-redHatDisplay font-bold"
               htmlFor="email"
@@ -37,10 +62,11 @@ function Register() {
               type="email"
               placeholder="stylzmc@gmail.com"
               id="email"
-              name="email"
-              autoComplete="username"
-              required
+              {...register("email", { required: "Email is required" })} // Register the input
             />
+            {errors.email && (
+              <span className="text-red-500">{errors.email.message}</span>
+            )}
             <label
               className="w-96 justify-start font-redHatDisplay font-bold"
               htmlFor="password"
@@ -52,10 +78,11 @@ function Register() {
               type="password"
               placeholder="********"
               id="password"
-              name="password"
-              autoComplete="new-password"
-              required
+              {...register("password", { required: "Password is required" })} // Register the input
             />
+            {errors.password && (
+              <span className="text-red-500">{errors.password.message}</span>
+            )}
             <label
               className="w-96 justify-start font-redHatDisplay font-bold"
               htmlFor="confirm_password"
@@ -67,11 +94,19 @@ function Register() {
               type="password"
               placeholder="********"
               id="confirm_password"
-              name="confirm_password"
-              autoComplete="new-password"
-              required
+              {...register("confirm_password", {
+                required: "Please confirm your password",
+              })} // Register the input
             />
-            <button className="btn my-2 w-full rounded-lg bg-accent py-2 text-lg text-white">
+            {errors.confirm_password && (
+              <span className="text-red-500">
+                {errors.confirm_password.message}
+              </span>
+            )}
+            <button
+              type="submit"
+              className="btn my-2 w-full rounded-lg bg-accent py-2 text-lg text-white"
+            >
               Register
             </button>
           </fieldset>
