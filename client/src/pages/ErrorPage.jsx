d36d094c67a5b24cf49800a5d1a1457459ab162a
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-02 20:34:41 +0200

    create error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,5 +1,21 @@
+import notFound from "../assets/not-found.svg";
+import ReturnBtn from "@/components/ReturnBtn";
+
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center w-full">
+        <img src={notFound} alt="404 Not Found" />
+      </div>
+      <h2 className="text-center mt-36 md:mt-12 font-bold text-4xl">Oops!</h2>
+      <p className="text-center py-6 text-2xl">
+        We couldn't find the page you're looking for
+      </p>
+      <div className="flex justify-center">
+        <ReturnBtn />
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
