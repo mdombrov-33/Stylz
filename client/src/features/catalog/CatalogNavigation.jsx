1f0f742db9ebe0f8fcab5add43174c58c67cea70
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-08 16:52:46 +0200

    finish filtering logic in catalog?

--- client/src/features/catalog/CatalogNavigation.jsx
+++ client/src/features/catalog/CatalogNavigation.jsx
@@ -1,4 +1,22 @@
-function CatalogNavigation({ setSelectedCategory, setSelectedGender }) {
+function CatalogNavigation({
+  selectedCategory,
+  setSelectedCategory,
+  selectedGender,
+  setSelectedGender,
+  setFetchTrigger,
+}) {
+  const handleGenderClick = (gender) => {
+    // Toggle selection for gender
+    setSelectedGender(selectedGender === gender ? null : gender);
+    setFetchTrigger(true);
+  };
+
+  const handleCategoryClick = (category) => {
+    // Toggle selection for category
+    setSelectedCategory(selectedCategory === category ? null : category);
+    setFetchTrigger(true);
+  };
+
   return (
     <aside className="drawer-side">
       <label
@@ -7,70 +25,88 @@ function CatalogNavigation({ setSelectedCategory, setSelectedGender }) {
         className="drawer-overlay"
       ></label>
       <ul className="menu bg-base-200 text-base-content mt-[105px] w-80 p-4">
-        {/* Sidebar content*/}
+        {/* Gender Filter Buttons */}
         <li>
           <button
-            onClick={() => setSelectedGender("men")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleGenderClick("men")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedGender === "men" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             men
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedGender("women")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleGenderClick("women")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedGender === "women" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             women
           </button>
         </li>
         <div className="divider divider-neutral mt-8"></div>
+
+        {/* Category Filter Buttons */}
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("shirts")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("shirts")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "shirts" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             shirts
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("jackets")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("jackets")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "jackets" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             jackets
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("pants")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("pants")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "pants" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             pants
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("sweaters")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("sweaters")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "sweaters" ? "btn-neutral" : "btn-outline"
+            }`}
           >
             sweaters
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("shoes")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("coats")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "coats" ? "btn-neutral" : "btn-outline"
+            }`}
           >
-            shoes
+            coats
           </button>
         </li>
         <li className="mt-4">
           <button
-            onClick={() => setSelectedCategory("coats")}
-            className="uppercase text-2xl font-semibold btn btn-outline"
+            onClick={() => handleCategoryClick("shoes")}
+            className={`uppercase text-2xl font-semibold btn ${
+              selectedCategory === "shoes" ? "btn-neutral" : "btn-outline"
+            }`}
           >
-            coats
+            shoes
           </button>
         </li>
       </ul>
--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -11,6 +11,7 @@ function Catalog() {
 
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedGender, setSelectedGender] = useState(null);
+  const [fetchTrigger, setFetchTrigger] = useState(true);
 
   const fetchCatalogItems = async ({ queryKey }) => {
     const [, { page, category, gender }] = queryKey;
@@ -35,10 +36,29 @@ function Catalog() {
     refetchOnWindowFocus: true,
     refetchInterval: 1000 * 60 * 15, // 15 minutes
     retry: 3,
+    enabled: fetchTrigger,
     keepPreviousData: true, // Keeps previous page data while fetching the new page
   });
-
   console.log(data);
+
+  // Refetch when category or gender change
+  useEffect(() => {
+    if (selectedCategory !== null || selectedGender !== null) {
+      setFetchTrigger(true);
+    }
+  }, [selectedCategory, selectedGender, page]);
+
+  // Reset fetchTrigger to false after the first fetch
+  useEffect(() => {
+    if (fetchTrigger && data) {
+      setFetchTrigger(false);
+    }
+  }, [fetchTrigger, data]);
+
+  // Reset page to 1 when filters change
+  useEffect(() => {
+    setPage(1);
+  }, [selectedCategory, selectedGender]);
 
   // Scroll to the top of the page when the page number changes
   useEffect(() => {
@@ -46,7 +66,7 @@ function Catalog() {
       top: 0,
       behavior: "smooth",
     });
-  }, [page]);
+  }, [page, selectedCategory, selectedGender]);
 
   const handleNextPage = () => {
     if (page < data.totalPages) setPage((prevPage) => prevPage + 1);
@@ -66,16 +86,18 @@ function Catalog() {
 
   console.log(selectedCategory, selectedGender);
 
-  const filteredItems = data.items.filter((item) => {
-    const isCategoryMatch = selectedCategory
-      ? item.category === selectedCategory
-      : true;
-    const isGenderMatch = selectedGender
-      ? item.gender === selectedGender
-      : true;
+  // Handle cases where data or data.items might be undefined
+  const filteredItems =
+    data?.items?.filter((item) => {
+      const isCategoryMatch = selectedCategory
+        ? item.category === selectedCategory
+        : true;
+      const isGenderMatch = selectedGender
+        ? item.gender === selectedGender
+        : true;
 
-    return isCategoryMatch && isGenderMatch;
-  });
+      return isCategoryMatch && isGenderMatch;
+    }) || [];
 
   return (
     <main className="drawer lg:drawer-open">
@@ -89,24 +111,28 @@ function Catalog() {
             <BiSolidCategory className="inline-block mr-2 text-3xl" />
           </label>
         </section>
+
         {/* Catalog Items */}
         <section className="flex flex-col items-center justify-start py-6">
           <section className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 gap-4 px-4 mt-6">
-            {filteredItems.map((item) => (
-              <CatalogItem
-                key={item.id}
-                isAvailable={item.isAvailable}
-                price={item.price}
-                name={item.name}
-                image={item.image}
-                altImage={item.altImage}
-              />
-            ))}
+            {data &&
+              filteredItems.map((item) => (
+                <CatalogItem
+                  key={item.id}
+                  isAvailable={item.isAvailable}
+                  price={item.price}
+                  name={item.name}
+                  image={item.image}
+                  altImage={item.altImage}
+                />
+              ))}
           </section>
         </section>
+
         {/* Pagination Controls */}
 
-        {filteredItems.length === 0 ? (
+        {/* No items found */}
+        {data && filteredItems.length === 0 ? (
           <section className="flex items-center justify-center h-96">
             <h1 className="text-4xl font-bold mt-10">
               No items found for current filters
@@ -114,7 +140,8 @@ function Catalog() {
           </section>
         ) : null}
 
-        {filteredItems.length > 0 && (
+        {/* Pagination */}
+        {data && filteredItems.length > 0 && (
           <section className="join grid grid-cols-2 mt-4 items-center justify-center p-6 w-full">
             <button
               onClick={handlePrevPage}
@@ -132,7 +159,9 @@ function Catalog() {
             </button>
           </section>
         )}
-        {filteredItems.length > 0 && (
+
+        {/* Track Pages */}
+        {data && filteredItems.length > 0 && (
           <p className="text-center">{`Page ${page} of ${data.totalPages}`}</p>
         )}
       </section>
@@ -142,6 +171,8 @@ function Catalog() {
         setSelectedCategory={setSelectedCategory}
         setSelectedGender={setSelectedGender}
         selectedGender={selectedGender}
+        selectedCategory={selectedCategory}
+        setFetchTrigger={setFetchTrigger}
       />
     </main>
   );
