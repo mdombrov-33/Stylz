c2306a4869f796a762852e7d2b70f38bf1c79779
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-10 10:33:16 +0200

    fix layout in catalog

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -65,7 +65,7 @@ function Cart() {
       </main>
 
       <section>
-        <h2 className="mt-6 text-center text-2xl font-bold">
+        <h2 className="mt-6 text-center text-2xl font-bold md:text-4xl">
           Total Price:{" "}
           {cart
             .reduce((acc, item) => acc + item.price * item.quantity, 0)
