34f772eb52189104f60db22bcaf12fcaa140d84d
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-12 20:43:05 +0200

    try to fix margin bug in catalog

--- client/src/features/catalog/CatalogContent.jsx
+++ client/src/features/catalog/CatalogContent.jsx
@@ -6,6 +6,7 @@ import useThemeStore from "@/store/theme-store";
 function CatalogContent({ id, isAvailable, price, name, image, altImage }) {
   const [isHovered, setIsHovered] = useState(false);
   const { theme } = useThemeStore((state) => state);
+
   const baseURL = "https://stylz-shop.onrender.com";
 
   return (
@@ -13,7 +14,7 @@ function CatalogContent({ id, isAvailable, price, name, image, altImage }) {
       <section className="card shadow-xl">
         <figure>
           <img
-            className={`${theme === "lemonade" ? "brightness-75 saturate-100" : "brightness-50 saturate-100"} max-h-96 w-full min-w-96 object-cover`}
+            className={`${theme === "lemonade" ? "brightness-75 saturate-100" : "brightness-50 saturate-100"} max-h-96 w-full object-cover`}
             src={`${baseURL}/${isHovered ? altImage : image}`}
             alt={name}
             onMouseLeave={() => setIsHovered(false)}
