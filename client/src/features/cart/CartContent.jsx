3c96672d861e718cae32d392fd6d1dbf3e230d54
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-12 21:35:38 +0200

    try to fix margin in cart

--- client/src/features/cart/CartContent.jsx
+++ client/src/features/cart/CartContent.jsx
@@ -10,7 +10,7 @@ function CartContent() {
 
   return (
     <main className="flex">
-      <table className="table">
+      <table className="table overflow-x-auto">
         <thead>
           <tr>
             <th className="font-delaGothicOne md:text-2xl">Item</th>
